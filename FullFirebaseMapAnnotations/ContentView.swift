5f66ff62f3cba3acb0b8c37f5f70329b36ebc252
Anish Rangdal <anish.rangdal@icloud.com>
2022-09-11 19:47:03 -0400

    Initial Commit

---
+++ FullFirebaseMapAnnotations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FullFirebaseMapAnnotations
+//
+//  Created by Anish Rangdal on 9/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FullFirebaseMapAnnotations/FullFirebaseMapAnnotationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullFirebaseMapAnnotationsApp.swift
+//  FullFirebaseMapAnnotations
+//
+//  Created by Anish Rangdal on 9/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FullFirebaseMapAnnotationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
